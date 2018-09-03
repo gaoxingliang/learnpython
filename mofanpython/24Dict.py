e774db1ce835b2ba82fafd9eceb514eaa6c7d812
edward.gao <scugxl@gmail.com>
2018-09-03 22:31:28 +0800

    refactor the dir

--- mofanpython/24Dict.py
+++ mofanpython/24Dict.py
@@ -12,3 +12,19 @@ print(dict["apple"])
 del dict["pear"]
 
 print(dict)
+
+
+
+char_list=['a', 'b', 'b', 'a']
+print(set(char_list))
+
+unique_char = set(char_list)
+unique_char.add("Q")
+print(unique_char)
+print("remove it", unique_char.remove('a'))
+print("discard ", unique_char.discard('Y'))
+
+sentence = "welcome to this demo"
+print(set(sentence))
+
+
