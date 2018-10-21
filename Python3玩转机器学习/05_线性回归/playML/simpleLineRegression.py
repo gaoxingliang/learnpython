f9cba0123fdbc869fc0738cec1643b04be057bd1
edward.gao <scugxl@gmail.com>
2018-10-21 16:02:10 +0800

    07- 多元线性回归

--- Python3玩转机器学习/05_线性回归/playML/simpleLineRegression.py
+++ Python3玩转机器学习/05_线性回归/playML/simpleLineRegression.py
@@ -39,3 +39,46 @@ class SimpleLinearRegression1:
 
     def __repr__(self):
         return "SimpleLinearRegression1"
+
+
+import numpy as np
+
+from .metrics import r2_score
+class SimpleLinearRegression2:
+    def __init__(self):
+        """初始化y=ax+b分别为None"""
+        self.a_ = None
+        self.b_ = None
+
+
+    def fit(self, x_train, y_train):
+        """目前只能处理1维的 所以要求2个都是1维的 而且要求他们的长度一样"""
+        """使用向量化运算"""
+        assert x_train.ndim == 1
+        assert y_train.ndim == 1
+        assert len(x_train) == len(y_train)
+
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+        fenzi = (x_train - x_mean).dot(y_train - y_mean)
+        fenmu = (x_train - x_mean).dot(x_train - x_mean)
+        self.a_ = fenzi / fenmu
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+
+    def predict(self, x_predict):
+        """要求x是一个向量"""
+        assert x_predict.ndim == 1
+        assert self.a_ is not None and self.b_ is not None
+        return np.array([self._predict(x) for x in x_predict])
+    def score(self, x_test, y_test):
+        y_predict = self.predict(x_test)
+        return r2_score(y_test, y_predict)
+
+    def _predict(self, x):
+        return self.a_ * x + self.b_
+
+
+    def __repr__(self):
+        return "SimpleLinearRegression2"
