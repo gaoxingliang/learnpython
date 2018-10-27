1a4dcf0eba0f784c558a1d968b50c01d18f0a347
edward.gao <scugxl@gmail.com>
2018-10-27 09:14:14 +0800

    04- 梯度下降实现

--- Python3玩转机器学习/06_梯度下降/playML/LinearRegression.py
+++ Python3玩转机器学习/06_梯度下降/playML/LinearRegression.py
@@ -11,11 +11,21 @@ class LinearRegression:
         self._theta = None
 
     def fit_normal(self, X_train, y_train):
+        # 使用正规方程解的计算方式
         assert X_train.shape[0] == y_train.shape[0], "样本数量必须一致"
         X_b = np.hstack([np.ones((len(X_train), 1), dtype="float"), X_train])
         self._theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y_train)
         self._updateCoefAndInterception()
         return self
+
+    def fit_normal_pesudo_inv(self, X_train, y_train):
+        # 使用正规方程解的计算方式 看看伪逆矩阵计算应该是一致的
+        assert X_train.shape[0] == y_train.shape[0], "样本数量必须一致"
+        X_b = np.hstack([np.ones((len(X_train), 1), dtype="float"), X_train])
+        self._theta = np.linalg.pinv(X_b).dot(y_train)
+        self._updateCoefAndInterception()
+        return self
+
     def _updateCoefAndInterception(self):
         self.interception_ = self._theta[0]
         self.coef_ = self._theta[1:]
@@ -34,14 +44,19 @@ class LinearRegression:
 
         def dJ(theta, X_b, y):
             # 计算偏导数 也就是梯度
-            res = np.empty(len(theta))
-            # 截距b的偏导数
-            res[0] = np.sum(X_b.dot(theta) - y)
+            # 方法1: 直接根据定义计算
+            # res = np.empty(len(theta))
+            # # 截距b的偏导数
+            # res[0] = np.sum(X_b.dot(theta) - y)
+            #
+            # # 对其他特征一个个计算 参考公式
+            # for i in range(1, len(theta)):
+            #     res[i] = np.sum((X_b.dot(theta) - y).dot(X_b[:, i])) / len(y_train)
+            # return res
 
-            # 对其他特征一个个计算 参考公式
-            for i in range(1, len(theta)):
-                res[i] = np.sum((X_b.dot(theta) - y).dot(X_b[:, i])) / len(y_train)
-            return res
+            # 方法2 直接向量化计算
+            return X_b.T.dot(X_b.dot(theta) - y) * 2 / len(y)
+
 
         def gd(X_b, y, initial_theta, eta, epsilon = 1e-8, n_iterations=1e4):
             n_iters = 0
