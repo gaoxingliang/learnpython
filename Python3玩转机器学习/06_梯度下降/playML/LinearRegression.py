76f50832b245a300cdabb11b7fda540fd92ef380
edward.gao <scugxl@gmail.com>
2018-11-01 22:55:47 +0800

    06-07- sgd梯度下降实现

--- Python3玩转机器学习/06_梯度下降/playML/LinearRegression.py
+++ Python3玩转机器学习/06_梯度下降/playML/LinearRegression.py
@@ -93,6 +93,46 @@ class LinearRegression:
         return self
 
 
+    # SGD
+    # a, b 用来计算eta 的值 随着迭代次数越来越小
+    def fit_sgd(self, X_train, y_train, n_iters=5, a=5, b = 50):
+        assert len(X_train) == len(y_train)
+        assert n_iters >= 1
+
+        def dJ_sgd(theta, X_b_i, y_i):
+            # X_b_i 代表第i个样本  y_i 代表数据值
+            return X_b_i.T.dot(X_b_i.dot(theta) - y_i) * 2.
+
+        def sgd(X_b, y, initial_theta, n_iters, a, b):
+            a = 5
+            b = 50
+
+            def learning_rate(iter):
+                return a / (b + iter)
+
+            # 因为我们是随机梯度下降 所以一般的话 我们至少要保证每个都看一遍
+            # 所以在sgd 中n_iters 一般表示所有数据最多看多少遍
+
+            # 现在是随机选一个点 可能这2个点隔的很近 所以epsilon 已经没有意义了
+            m = len(X_b)
+            theta = initial_theta
+            for cur_iter in range(n_iters):
+                # 为了保证每个都看过 所以我们打乱这个原来的X_b
+                indexes = np.random.permutation(m)
+                X_b_new = X_b[indexes]
+                y_new = y[indexes]
+                for i in range(m):
+                    gradient = dJ_sgd(theta, X_b_new[i], y_new[i])
+                    theta = theta - learning_rate(cur_iter * m + i) * gradient
+            return theta
+
+        X_b = np.hstack((np.ones((len(X_train), 1)), X_train))
+        initial_theta = np.zeros(X_b.shape[1])
+        # 这里只是举例取1/3的数据
+        self._theta = sgd(X_b, y_train, initial_theta, n_iters, a, b)
+        self._updateCoefAndInterception()
+    # SGD
+
 
 
     def predict(self, X_predict):
